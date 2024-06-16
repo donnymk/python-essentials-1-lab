edd6f5f9e219e8425d9bdc90a1699b891534bf6d
Donny Malik Kurniawan <donnymalikkurniawan@gmail.com>
2024-06-16 12:36:24 +0700

    operators and expressions

---
+++ 4-aritmetika-dasar.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first value: "))
+b = float(input("Enter second value: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("\nThat's all, folks!")
+
